fe3eeb94be99c7306f411d6934cd13a94ba3759c
Jay F <85573433+jayflan@users.noreply.github.com>
2024-05-15 09:26:53 -0400

    initial commit of section

---
+++ array_cardio_day/app.spec.js
@@ -0,0 +1,11 @@
+// const { expect } = require('chai');
+// const app = require('./app');
+
+//deativated, moved to testing to front end in index.html file
+// describe("Array Cardio Tests", () => {
+//   describe("Inventor Filter", () => {
+//     it("Filter Inventors Born in 1500's", () => {
+//       expect(inv);
+//     });
+//   });
+// });
