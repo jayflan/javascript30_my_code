050cf24065c45065d47ef511fe099cf2866ee451
Jay F <85573433+jayflan@users.noreply.github.com>
2024-05-21 16:44:40 -0400

    WIP

--- 06_ajax_type_ahead/index.js
+++ 06_ajax_type_ahead/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index-START.html'));
 });
 
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'style.css'));
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
