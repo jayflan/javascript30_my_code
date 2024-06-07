e8a744820ee3b5f2982084c020ca6ea4bf1a4b06
Jay F <85573433+jayflan@users.noreply.github.com>
2024-06-07 14:30:27 -0400

    finished exercise

---
+++ 09_Dev_Tools_Domination/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index-START.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
