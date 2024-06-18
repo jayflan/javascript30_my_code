a99e6c8829c72e6f3772fdec04d3ef545bedf307
Jay F <85573433+jayflan@users.noreply.github.com>
2024-06-18 11:53:21 -0400

    finished all controls

--- 11_Custom_HTML5_Video_Player/scripts.js
+++ 11_Custom_HTML5_Video_Player/scripts.js
@@ -1,26 +1,47 @@
 let isPlaying = false;
+let isDragging = false;
 
 const video = document.querySelector(".viewer");
 const playButton = document.querySelector(".toggle");
 const skipButtons = document.querySelectorAll('[data-skip]');
 const ranges = document.querySelectorAll('.player__slider');
+const progressBar = document.querySelector(".progress");
 
-//video progress bar appearance
+//video progress bar filled appearance
 video.addEventListener('timeupdate', function () {
+    progressFilled = document.querySelector(".progress__filled");
     if (this.currentTime === this.duration) {
         this.pause();
     };
     const percentage = (this.currentTime / this.duration) * 100;
-    const progressBar = document.querySelector(".progress__filled");
-    progressBar.style.flexBasis = `${percentage}%`;
+    progressFilled.style.flexBasis = `${percentage}%`;
 
 });
 
 //drag progress bar to specific time of playback
-
-
+progressBar.addEventListener('click', (event) => {
+    const xpos = event.offsetX;
+    const barWidth = progressBar.offsetWidth;
+    handleProgressBar(xpos, barWidth, video);
+});
+progressBar.addEventListener('mousedown', (event) => {
+    isDragging = true;
+   progressBar.style.cursor = 'grabbing';
+   
+});
+progressBar.addEventListener('mouseup', () => {
+    isDragging = false;
+    progressBar.style.cursor = 'ew-resize';
+});
+progressBar.addEventListener('mousemove', (event) => {
+    if(!isDragging) return;
+    const xpos = event.offsetX;
+    const barWidth = progressBar.offsetWidth;
+    handleProgressBar(xpos, barWidth, video);
+})
 //click & drag volume or playbackRate sliders
 ranges.forEach(range => {
+    handleSlider(range, video); //set initial range tool-title values
     range.addEventListener('change', () => {
         handleSlider(range, video);
     });
@@ -36,6 +57,10 @@ skipButtons.forEach(button => {
     });
 });
 
+function handleProgressBar(pos, barWidth, video) {
+    const updatedTime = (pos / barWidth) * video.duration;
+    video.currentTime = updatedTime;
+};
 
 
 function handleTogglePlay(button, videoViewer) {
@@ -54,14 +79,18 @@ function handleTogglePlay(button, videoViewer) {
 };
 
 function handleSkipButtons(buttons, videoViewer) {
-    console.log(buttons.dataset.skip);
     const skipSeconds = parseInt(buttons.dataset.skip);
     videoViewer.currentTime += skipSeconds;    
 };
 
 function handleSlider(slider, video) {
     const videoView = video, name = slider.name;
-    videoView[name] = slider.value;
-    slider.title = `${name}: ${videoView[name]}`;
+    if(name === "volume") {
+        videoView[name] = slider.value;
+        slider.title = `${name}: ${videoView[name] * 10}`;
+    } else {
+        videoView[name] = slider.value;
+        slider.title = `${name}: ${videoView[name]}`;
+    };
 };
 
