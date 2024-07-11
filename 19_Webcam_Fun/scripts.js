5e6fad85311dbd5f0360ce3a7c1a7f216d4b9cd9
Jay F <85573433+jayflan@users.noreply.github.com>
2024-07-11 12:31:59 -0400

    wip, camera working

--- 19_Webcam_Fun/scripts.js
+++ 19_Webcam_Fun/scripts.js
@@ -1,5 +1,30 @@
 const video = document.querySelector('.player');
+video.hidden = true;
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+const startWebcam = async() => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+    
+    video.srcObject = stream;
+    video.play();
+
+    video.addEventListener('loadedmetadata', () => {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      renderWebcamFrame();
+    });
+
+    const renderWebcamFrame = () => {
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      requestAnimationFrame(renderWebcamFrame);
+    };
+  } catch(err) {
+    console.log('Failed to get webcam stream', err);
+  };
+};
+
+startWebcam();
